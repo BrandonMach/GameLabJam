8f237f3dd9bb41f4b3ffcfd03878e9ec234b0204
MartinGuva <martin.guvaa@gmail.com>
2023-04-01 12:57:13 +0200

    some changes

--- Game LabJ am Grupp 6/Assets/Martin/Scripts/ElementalTransform.cs
+++ Game LabJ am Grupp 6/Assets/Martin/Scripts/ElementalTransform.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ElementalTransform : MonoBehaviour
 {
+    public enum ElementalState { gas = 0, water = 1, ice = 2};
+    public ElementalState currentState;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,28 @@ public class ElementalTransform : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (currentState == ElementalState.gas)
+        {
+
+        }
+        else if (currentState == ElementalState.water)
+        {
+
+        }
+        else if (currentState == ElementalState.ice)
+        {
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Debug.Log(currentState);
+            currentState++;
+            if (currentState == ElementalState.ice)
+            {
+                currentState = 0;
+            }
+            Debug.Log(currentState);
+        }
     }
 }
