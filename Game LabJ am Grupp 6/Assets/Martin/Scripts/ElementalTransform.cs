417ca7f534c151d9949ad3605a38d1e668b1e047
MartinGuva <martin.guvaa@gmail.com>
2023-04-01 13:26:56 +0200

    update

--- Game LabJ am Grupp 6/Assets/Martin/Scripts/ElementalTransform.cs
+++ Game LabJ am Grupp 6/Assets/Martin/Scripts/ElementalTransform.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ElementalTransform : MonoBehaviour
 {
     public enum ElementalState { gas = 0, water = 1, ice = 2};
     public ElementalState currentState;
+
+    private float iceSizeX = 2;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,28 +18,47 @@ public class ElementalTransform : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (currentState == ElementalState.gas)
-        {
 
-        }
-        else if (currentState == ElementalState.water)
-        {
 
-        }
-        else if (currentState == ElementalState.ice)
+        if (Input.GetKeyDown(KeyCode.Alpha0))
         {
-
-        }
-
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            Debug.Log(currentState);
             currentState++;
-            if (currentState == ElementalState.ice)
+            if (currentState > ElementalState.ice)
             {
                 currentState = 0;
             }
-            Debug.Log(currentState);
+            ChangeState(currentState);
         }
+    }
+
+    public void ChangeState(ElementalState currentState)
+    {
+        if (currentState == ElementalState.gas)
+        {
+            GasState();
+        }
+        else if (currentState == ElementalState.water)
+        {
+            WaterState();
+        }
+
+        else if (currentState == ElementalState.ice)
+        {
+            IceState();
+        }
+    }
+
+    public void WaterState()
+    {
+    }
+
+    public void IceState()
+    {
+        this.transform.localScale = new Vector3(iceSizeX, transform.localScale.y, 0);
+    }
+
+    public void GasState()
+    {
+        this.transform.localScale = new Vector3(1, transform.localScale.y, 0);
     }
 }
