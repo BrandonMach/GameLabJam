acd0429779f2631f9c093fc8408263e734fe7ab7
BrandonMach <brandon.mach2003@gmail.com>
2023-04-02 11:41:32 +0200

    Pushing animation

--- Game LabJ am Grupp 6/Assets/BrandonScripts/PlayerController.cs
+++ Game LabJ am Grupp 6/Assets/BrandonScripts/PlayerController.cs
@@ -6,14 +6,24 @@ public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     public List<Animator> anim;
+    public GameObject[] characters;
     PlayerMovment playerMovementScript;
     void Start()
     {
         playerMovementScript = this.GetComponent<PlayerMovment>();
     }
+    public void Awake()
+    {
+        anim = new List<Animator>();
+        for (int i = 0; i < characters.Length; i++)
+        {
+            anim.Add(characters[i].GetComponent<Animator>());
+        }
+    }
 
-    // Update is called once per frame
-    void Update()
+
+        // Update is called once per frame
+        void Update()
     {
         //anim 0 = walk animation
         for (int i = 0; i < anim.Count; i++)
